603a3c587e56ca08e0ed0580f03c4dee2bab1bf4
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-06-07 13:25:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,64 @@ import os
 
 version = '2.1.0.dev19+auction.worker.sharding'
 
+install_requires = [
+    'setuptools',
+    'requests',
+    'APScheduler',
+    'iso8601',
+    'python-dateutil',
+    'Flask',
+    'flask-cors',
+    'WTForms',
+    'WTForms-JSON',
+    'Flask-Redis',
+    'WSGIProxy2',
+    'gevent',
+    'sse',
+    'flask_oauthlib',
+    'PyYAML',
+    'request_id_middleware',
+    'restkit',
+    'PyMemoize',
+    'barbecue',
+    # ssl warning
+    'pyopenssl',
+    'ndg-httpsclient',
+    'pyasn1',
+    'openprocurement_client',
+    'python-consul',
+    'retrying',
+    'reg',
+    'dectate'
+]
+
+
+extra_requires = {
+    'test': [
+        'robotframework',
+        'robotframework-selenium2library',
+        'robotframework-debuglibrary',
+        'robotframework-selenium2screenshots',
+        'chromedriver',
+        'mock'
+    ]
+}
+
+
+entry_points = {
+    'openprocurement.auction.plugins': [
+        'simple = openprocurement.auction.auction:Auction',
+    ],
+    'console_scripts': [
+        'auction_worker = openprocurement.auction.auction_worker:main',
+        'auctions_chronograph = openprocurement.auction.chronograph:main',
+        'auctions_data_bridge = openprocurement.auction.databridge:main',
+        'auction_test = openprocurement.auction.tests.main:main [test]'
+    ],
+    'paste.app_factory': [
+        'auctions_server = openprocurement.auction.auctions_server:make_auctions_app',
+    ]
+}
 
 setup(name='openprocurement.auction',
       version=version,
@@ -21,59 +79,10 @@ setup(name='openprocurement.auction',
       license='Apache License 2.0',
       url='https://github.com/openprocurement/openprocurement.auction',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['openprocurement'],
+      namespace_packages=['openprocurement', 'openprocurement.auction'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'requests',
-          'APScheduler',
-          'iso8601',
-          'python-dateutil',
-          'Flask',
-          'flask-cors',
-          'WTForms',
-          'WTForms-JSON',
-          'Flask-Redis',
-          'WSGIProxy2',
-          'gevent',
-          'sse',
-          'flask_oauthlib',
-          'Flask-Assets',
-          'cssmin',
-          'jsmin',
-          'PyYAML',
-          'request_id_middleware',
-          'restkit',
-          'PyMemoize',
-          'barbecue',
-          # ssl warning
-          'pyopenssl',
-          'ndg-httpsclient',
-          'pyasn1',
-          'openprocurement_client',
-          'python-consul',
-          'retrying',
-      ],
-      extras_require={
-          'test': [
-              'robotframework',
-              'robotframework-selenium2library',
-              'robotframework-debuglibrary',
-              'robotframework-selenium2screenshots',
-              'chromedriver',
-              'mock'
-          ]
-      },
-      entry_points={
-          'console_scripts': [
-              'auction_worker = openprocurement.auction.auction_worker:main',
-              'auctions_chronograph = openprocurement.auction.chronograph:main',
-              'auctions_data_bridge = openprocurement.auction.databridge:main',
-              'auction_test = openprocurement.auction.tests.main:main [test]'
-          ],
-          'paste.app_factory': [
-              'auctions_server = openprocurement.auction.auctions_server:make_auctions_app',
-          ]
-      },
-      )
+      install_requires=install_requires,
+      extras_require=extra_requires,
+      entry_points=entry_points
+)
