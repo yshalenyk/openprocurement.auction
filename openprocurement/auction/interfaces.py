afd69d95f52d567a644b937030b74c048f8eb829
Yaroslav Shalenyk <yshalenyk@gmail.com>
2017-06-12 10:43:06 +0300

    Added interfaces

---
+++ openprocurement/auction/interfaces.py
@@ -0,0 +1,36 @@
+from zope.interface import Interface
+
+
+class IAuction(Interface):
+    """"""
+
+    
+class IAuctionDatabridge(Interface):
+    """"""
+
+    
+class IAuctionDocument(Interface):
+    """"""
+
+    
+class IAuctionWorker(Interface):
+    """"""
+
+    
+class IWorkerFactory(Interface):
+    """"""
+
+
+class IAuctionLogger(Interface):
+    """"""
+    
+
+class IAuctionMangager(Interface):
+    """"""
+
+    
+class IPredicateRegistry(Interface):
+    """"""
+
+class IRegistry(Interface):
+    """"""
